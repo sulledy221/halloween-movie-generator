2e741834e21335f06b0bc46405922941333c4323
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-08-29 20:26:49 -0700

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import './App.css';
-import Halloween from './components/Halloween';
-import * as movies from './components/movies'
+import "./App.css";
+import Halloween from "./components/Halloween";
+import * as movies from "./components/movies";
 
 function App() {
   return (
     <div className="App">
       <h1>This is Halloween</h1>
-        <Halloween halloweenItems={movies.Halloween70} type="Spookies from the 70's" className="movie-text"/>
-        <Halloween halloweenItems={movies.Halloween80} type="Spookies from the 80's" className="movie-text"/>
-        <Halloween halloweenItems={movies.Halloween90} type="Spookies from the 90's" className="movie-text"/>
-        <Halloween halloweenItems={movies.HalloweenFamily} type="Family Spookies" className="movie-text"/>
-        
+      <Halloween
+        halloweenItems={movies.Halloween70}
+        type="Spookies from the 70's"
+        className="movie-text"
+      />
+      <Halloween
+        halloweenItems={movies.Halloween80}
+        type="Spookies from the 80's"
+        className="movie-text"
+      />
+      <Halloween
+        halloweenItems={movies.Halloween90}
+        type="Spookies from the 90's"
+        className="movie-text"
+      />
+      <Halloween
+        halloweenItems={movies.HalloweenFamily}
+        type="Family Spookies"
+        className="movie-text"
+      />
     </div>
   );
 }
